5a54dfc6ccff4ab9c176d942b22442a1dfa77763
Kevin <kmoens@lakeco.com>
2022-03-05 21:11:55 -0600

    fix build issues

---
+++ EllaSciFair/Pages/Index.razor.cs
@@ -0,0 +1,16 @@
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.JSInterop;
+using EllaSciFair;
+
+namespace EllaSciFair.Pages
+{
+    public partial class Index : ComponentBase {
+
+    }
+}
